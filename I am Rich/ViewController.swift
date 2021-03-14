a32f8ebcad678e516b6de0ffbcc4e7ca6e7d7b65
Masateru maegawa <masa.mg283@gmail.com>
2021-03-14 20:37:33 +0900

    add files

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by m.m on 2020/10/21.
+//  Copyright © 2020 masa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
